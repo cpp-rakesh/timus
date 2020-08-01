f10dad304eae8d43ee0f282eab71be211aa30d79
Rakesh Kumar <cpp.rakesh@gmail.com>
2020-08-02 00:58:02 +1000

    1000

---
+++ 1000.cpp
@@ -0,0 +1,18 @@
+/*
+  A+B Problem
+  Rakesh Kumar
+  02/08/2020
+ */
+
+#include <bits/stdc++.h>
+
+int main() {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0); std::cout.tie(0);
+
+    int a = 0, b = 0;
+    std::cin >> a >> b;
+    std::cout << a + b << std::endl;
+
+    return 0;
+}
